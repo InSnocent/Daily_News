3ba6694aac8dee28968753642f8f9f74be426a45
InSnocent <wuxie1024@qq.com>
2022-07-27 17:14:19 +0800

    更换API

--- getPic.py
+++ getPic.py
@@ -7,28 +7,18 @@ import urllib.request
 from hoshino import Service
 from hoshino.typing import CQEvent
 
-api = 'https://api.vvhan.com/api/60s'
+api = 'https://api.03c3.cn/zb/'
 file_path = './hoshino/modules/Daily_News/imgs'
 file_me = '60s'
 
 def download_image():
     image_url = api
     print('正在下载资源')
-    def callback(a,b,c):  
-        '''
-        @a:已经下载的数据块 
-        @b:数据块的大小 
-        @c:远程文件的大小 
-        '''  
-        per=100.0*a*b/c  
-        if per>100:  
-            per=100  
-        print('%.2f%%' % per)
     try:
         if not os.path.exists(file_path):
             os.makedirs(file_path)            
         #下载保存图片
-        file_suffix = os.path.splitext(image_url)[1]
+        #file_suffix = os.path.splitext(image_url)[1]
         file_name = '{}{}{}{}'.format(file_path,os.sep,'today','.png') 
         urllib.request.urlretrieve(image_url,filename=file_name,reporthook=callback) 
         print("Download Successful！")
