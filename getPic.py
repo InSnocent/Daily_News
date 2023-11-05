397791ca05cd660d392de4a6ee30cf595c068234
InSnocent <wuxie1024@qq.com>
2023-11-05 19:57:40 +0800

    更改了获取图片的方式为异步，更新api

--- getPic.py
+++ getPic.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-import os,stat
-import requests
+import os
 import json
 import time
-import urllib.request
+import aiohttp
+import aiofiles
 from hoshino import Service
 from hoshino.typing import CQEvent
 
@@ -11,47 +11,50 @@ api = 'https://api.03c3.cn/api/zb'
 file_path = './hoshino/modules/Daily_News/imgs'
 file_me = '60s'
 
-def download_image():
-    # response = requests.get(api)
-    # loads = json.loads(response.text)
-    # image_url = loads.get('imageUrl')
+async def download_image():
     image_url = api
     print('正在下载资源')
     try:
         if not os.path.exists(file_path):
-            os.makedirs(file_path)            
-        #下载保存图片
-        #file_suffix = os.path.splitext(image_url)[1]
-        file_name = '{}{}{}{}'.format(file_path,os.sep,'today','.png') 
-        urllib.request.urlretrieve(image_url,filename=file_name) 
-        print("Download Successful！")
-    except IOError as e:
-        print("IOError,Please check on")
+            os.makedirs(file_path)
+        file_name = os.path.join(file_path, 'today.png')
+        async with aiohttp.ClientSession() as session:
+            async with session.get(image_url) as response:
+                if response.status == 200:
+                    async with aiofiles.open(file_name, 'wb') as f:
+                        while True:
+                            chunk = await response.content.read(1024)
+                            if not chunk:
+                                break
+                            await f.write(chunk)
+                    print("Download Successful！")
+                else:
+                    print("Failed to download image")
     except Exception as e:
-        print("Exception")
-
+        print("Exception:", str(e))
 
 if __name__ == '__main__':
-    download_image()
+    import asyncio
+    asyncio.run(download_image())
 
 sv = Service('Daily_News', enable_on_default=False)
 
-@sv.on_fullmatch(('今日新闻','每日新闻','新闻60秒','新闻60s'))
-async def Daily_News(bot, ev:CQEvent):
-    tdimg = 'today' + ".png" 
-    image_path = os.path.join(os.path.dirname(__file__),'imgs/',tdimg)
-    print (image_path)
+@sv.on_fullmatch(('今日新闻', '每日新闻', '新闻60秒', '新闻60s'))
+async def Daily_News(bot, ev: CQEvent):
+    tdimg = 'today' + ".png"
+    image_path = os.path.join(os.path.dirname(__file__), 'imgs/', tdimg)
+    print(image_path)
     try:
-        download_image()
+        await download_image()
         await bot.send(ev, f'[CQ:image,file=file:///{image_path}]')
         time.sleep(2)
-        await bot.send(ev,'数据来源:澎湃、人民日报、腾讯新闻、网易新闻、新华网、中国新闻网；每日凌晨1时后更新')
+        await bot.send(ev, '数据来源:澎湃、人民日报、腾讯新闻、网易新闻、新华网、中国新闻网；每日凌晨1时后更新')
     except:
-        await bot.send(ev,'获取失败，请重试或联系管理员')
+        await bot.send(ev, '获取失败，请重试或联系管理员')
 
 @sv.scheduled_job('cron', hour='09', minute='30', jitter=50)
 async def autoNews():
     tdimg = 'today' + ".png"
-    image_path = os.path.join(os.path.dirname(__file__),'imgs/',tdimg)
-    download_image()
+    image_path = os.path.join(os.path.dirname(__file__), 'imgs/', tdimg)
+    await download_image()
     await sv.broadcast(f'[CQ:image,file=file:///{image_path}]')
